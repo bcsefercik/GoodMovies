c134afc233527b02807249d5186ae04c103ecd39
Bugra Can Sefercik <bsefercik@gmail.com>
2016-08-24 08:53:58 +0300

    try

--- GoodMovies/User.swift
+++ GoodMovies/User.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Firebase
 
-class User {
+class UserTransactions {
     private let database = DatabaseAdapter()
     func addMovie(){
         database.insert("tt131352", path: "users/\((FIRAuth.auth()?.currentUser?.uid)!)", values: ["title": "The Social Network"])
